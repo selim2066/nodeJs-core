43a832a1cffc8c83795823d33bad4b0f208bd16a
selim2066 <mdselimreza2066@gmail.com>
2025-11-23 12:35:18 +0600

    foldername change just

--- module10/output/greetings.js
+++ module10/output/greetings.js
@@ -1,2 +1,21 @@
 console.log("version node: ", process.version)
 console.log("platform : ", process.platform)
+
+const args = process.argv
+
+// process.arg[0]= node path
+// process.arg[1]= file path
+// process.arg[2]= first actual path
+
+
+const name = args[2] || "guest"
+const time = new Date().getHours()
+
+if (time > 18) {
+  console.log(`good afternoon ${name}`)
+
+} else if (time < 12) {
+  console.log(`good morning ${name}`)
+} else {
+  console.log(`good evening ${name}`)
+}
